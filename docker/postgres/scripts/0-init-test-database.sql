55f931597c8562218d1efc91c76b2574a29c88cc
TudorOrban <tudororban2@gmail.com>
2026-02-04 12:13:47 +0200

    #0 Further adjust Github Workflow to wait for Postgres container to start x2

--- docker/postgres/scripts/0-init-test-database.sql
+++ docker/postgres/scripts/0-init-test-database.sql
@@ -1,12 +1,3 @@
 DROP DATABASE IF EXISTS threeangle_test;
 CREATE DATABASE threeangle_test;
-
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'threeangle') THEN
-        CREATE ROLE threeangle WITH LOGIN PASSWORD 'threeangle';
-    END IF;
-END
-$$;
-
-GRANT ALL PRIVILEGES ON DATABASE threeangle_test TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE threeangle_test TO threeangle;
